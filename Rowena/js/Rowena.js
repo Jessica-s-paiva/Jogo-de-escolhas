df65acf352d1f9dec4168aac2499e9b0b582fae8
Jessica-s-paiva <jessicapaivamvp@gmail.com>
2022-03-13 17:21:31 -0300

    criando interações do primeiro personagem em js

---
+++ Rowena/js/Rowena.js
@@ -0,0 +1,8 @@
+function responder() {
+    let resposta = prompt('Qual a alternativa correta?', 'A ou B');
+    if(resposta != null){
+        if(resposta == a){
+            window.location.assign()
+        }
+    }
+}
