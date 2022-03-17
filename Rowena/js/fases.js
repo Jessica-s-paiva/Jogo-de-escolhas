e90341957881939e15cc6abb9c7404662237bc81
Jessica-s-paiva <jessicapaivamvp@gmail.com>
2022-03-17 20:39:25 -0300

    criei funções para o usuario responder as questões e para avanço de fase

--- Rowena/js/fases.js
+++ Rowena/js/fases.js
@@ -1,33 +1,46 @@
-var contador = 0;
-
-function contar(){
-    contador++;
-    return contador;
-}
-
-function responder() {
+function responder1() {
     var resposta = prompt('Qual a alternativa correta?', 'A ou B');
     if(resposta != null){
         if(resposta == 'a'){
-           window.location.assign('errado.html');
-        }else if(resposta =='b'){
+            window.location.assign('errado.html');
+        }else if(resposta =='b'){            
             window.location.assign('certo.html');
-            return contador;
         }else{
             alert('Digite uma resposta válida =D')
         }
     }
 }
 
-function avanco() {
-    contar();
-    alert(contador)
-    if(contador == 1){
-        contar();
-        window.location.assign('fase-2.html');
-        alert(contador);
-        if(contador == 2){
-            window.location.assign('fase-3.html');
+function responder2() {
+    var resposta = prompt('Qual a alternativa correta?', 'A ou B');
+    if(resposta != null){
+        if(resposta == 'a'){
+            window.location.assign('errado.html');
+        }else if(resposta =='b'){            
+            window.location.assign('certo-1.html');
+        }else{
+            alert('Digite uma resposta válida =D')
         }
     }
+}
+
+function responder3() {
+    var resposta = prompt('Qual a alternativa correta?', 'A ou B');
+    if(resposta != null){
+        if(resposta == 'a'){
+            window.location.assign('errado.html');
+        }else if(resposta =='b'){            
+            window.location.assign('final.html');
+        }else{
+            alert('Digite uma resposta válida =D')
+        }
+    }
+}
+
+function proximaFase() {
+    window.location.assign('fase-2.html');
+}
+
+function proximaFase1() {
+    window.location.assign('fase-3.html');
 }
